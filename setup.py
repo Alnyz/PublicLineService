1c4e20f3036700c8d9adb19871440f619c0ef38d
Dyseo <katro.coplax@gmail.com>
2020-03-17 13:10:37 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
 	install_requires=['frugal'],
 	classifiers=[
 		"Programming Language :: Python",
-		"Programming Language :: Python :: 2.7",
 		"Programming Language :: Python :: 3.5",
 		"Programming Language :: Python :: 3.6",
 		"Programming Language :: Python :: 3.7",
